db657920bce3be9dbe689fc1df503acb640ef402
Courtney Chu <CourtneyHChu@gmail.com>
2017-08-03 21:41:37 +0000

    finally finished day 4

--- express_server.js
+++ express_server.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080;
+var cookieParser = require('cookie-parser'); // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 app.set("view engine", "ejs")
+
+//cookie-parser - helps us read the values from the cookie
+
+//res.cookie helps us set the values on the cookie
+
+//helps us set up the values to the cookie
+app.post("/login", (req, res) => {
+let name = req.body.username;
+res.cookie('username', name)
+res.redirect("/urls")
+});
+
+
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,20 +31,9 @@ var urlDatabase = {
 
 
 
-
-
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
 
 
 
@@ -43,7 +48,7 @@ function generateRandomString() {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {urls: urlDatabase, usernames: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -53,24 +58,43 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = newURL;
   console.log(urlDatabase);  // debug statement to see POST parameters
   res.redirect(`/urls/${shortURL}`);
-       // Respond with 'Ok' (we will replace this)
+
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  var longURL = urlDatabase[req.params.shortURL];
-  // let longURL = ...
-  res.redirect(longURL);
+//this should delete entries
+app.post("/urls/:id/delete", (req, res) => {
+delete urlDatabase[req.params.id];
+res.redirect('/urls/');
 });
 
 
+//This should let the user edit the URL and then direct them to the urls directory page
+// what comes in after the req.params and urls/: could have been anything. Represents id or short url in this case
+app.get("/u/:id", (req, res) => {
+var longURL = urlDatabase[req.params.id];
+res.redirect(longURL);
+});
 
+//edits urls
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect('/urls/');
+});
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+});
+
+//handles new urls
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { usernames: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
+//just rendering the page
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], usernames: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
